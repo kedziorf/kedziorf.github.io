6ebcb536ce9b2c651c96a1c21faab4ff3896b88d
kedziorf <filipkedzior@gmail.com>
2024-05-01 15:50:49 +0200

    qpa

---
+++ Mapp/src/components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+  <header>
+        <h1 style="margin-top: 14px;margin-left: 20px;"> mApp 🗺<button class="btn float-end py-1" type="button" style="color: white;background: #00a693;margin-right: 20px;">Log out</button></h1>
+    </header>
+</template>
